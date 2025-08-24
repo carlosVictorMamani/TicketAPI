1077e4eee66f101af3e0780436d59f87e43282eb
Carlos Victor Mamani Guevara <cvmamani@indracompany.com>
2025-08-24 16:42:18 -0500

    nuevo Desarrollo 240820251642

--- src/test/java/com/minsait/comunidad/ticket/services/TicketServicesTest.java
+++ src/test/java/com/minsait/comunidad/ticket/services/TicketServicesTest.java
@@ -5,16 +5,17 @@ import com.minsait.comunidad.ticket.dto.TicketDto;
 import com.minsait.comunidad.ticket.enums.Estado;
 import com.minsait.comunidad.ticket.mapper.TicketMapper;
 import com.minsait.comunidad.ticket.repository.TicketRepository;
-import com.minsait.comunidad.ticket.util.Utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import java.time.LocalDateTime;
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-
+@SpringBootTest
 class TicketServicesImplTest {
 
     @Mock
