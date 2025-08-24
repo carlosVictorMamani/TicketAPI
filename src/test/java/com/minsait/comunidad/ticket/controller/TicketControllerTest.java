3e634365004bc15f4cddb5d323186bf299d4adc8
Carlos Victor Mamani Guevara <cvmamani@indracompany.com>
2025-08-24 14:02:35 -0500

    nuevo Desarrollo 240820251359

--- src/test/java/com/minsait/comunidad/ticket/controller/TicketControllerTest.java
+++ src/test/java/com/minsait/comunidad/ticket/controller/TicketControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.minsait.comunidad.ticket.dto.TicketDto;
 import com.minsait.comunidad.ticket.services.TicketServices;
 
 import java.util.Arrays;
@@ -34,5 +35,21 @@ public class TicketControllerTest {
     public void testlist() throws Exception  {
        
     }
+    @Test
+    public void testListReturnsOkAndTickets() throws Exception {
+        TicketDto ticket1 = new TicketDto();
+        ticket1.setCodigo("123");
+        TicketDto ticket2 = new TicketDto();
+        ticket2.setCodigo("456");
+        List<TicketDto> tickets = Arrays.asList(ticket1, ticket2);
 
+        when(service.findAll()).thenReturn(tickets);
+
+        mockMvc.perform(get("/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].codigo", is("123")))
+                .andExpect(jsonPath("$[1].codigo", is("456")));
+    }
 }
