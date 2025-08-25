452a37f9fa86f7baf284524ae29ca1a3f4cdd8d3
Carlos Victor Mamani Guevara <cvmamani@indracompany.com>
2025-08-25 04:49:07 -0500

    nuevo desarrollo 24082025v13

--- src/main/java/com/minsait/comunidad/ticket/dto/TicketDto.java
+++ src/main/java/com/minsait/comunidad/ticket/dto/TicketDto.java
@@ -9,11 +9,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
-
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TicketDto implements java.io.Serializable {
     
-    private static final long serialVersionUID = 42L;
-
     private String id;
     private String codigo;
     private long orden;
@@ -24,90 +26,4 @@ public class TicketDto implements java.io.Serializable {
     private Estado estado;
     private LocalDateTime fechaCreacion;
 
-    public TicketDto() {
-    }
-
-    public TicketDto(String id, String codigo, long orden, String comentario, String descripcion, String solicitante, String usuarioGenerador, Estado estado, LocalDateTime fechaCreacion) {
-        this.id = id;
-        this.codigo = codigo;
-        this.orden = orden;
-        this.comentario = comentario;
-        this.descripcion = descripcion;
-        this.solicitante = solicitante;
-        this.usuarioGenerador = usuarioGenerador;
-        this.estado = estado;
-        this.fechaCreacion = fechaCreacion;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public long getOrden() {
-        return orden;
-    }
-
-    public void setOrden(long orden) {
-        this.orden = orden;
-    }
-
-    public String getComentario() {
-        return comentario;
-    }
-
-    public void setComentario(String comentario) {
-        this.comentario = comentario;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getSolicitante() {
-        return solicitante;
-    }
-
-    public void setSolicitante(String solicitante) {
-        this.solicitante = solicitante;
-    }
-
-    public String getUsuarioGenerador() {
-        return usuarioGenerador;
-    }
-
-    public void setUsuarioGenerador(String usuarioGenerador) {
-        this.usuarioGenerador = usuarioGenerador;
-    }
-
-    public Estado getEstado() {
-        return estado;
-    }
-
-    public void setEstado(Estado estado) {
-        this.estado = estado;
-    }
-
-    public LocalDateTime getFechaCreacion() {
-        return fechaCreacion;
-    }
-
-    public void setFechaCreacion(LocalDateTime fechaCreacion) {
-        this.fechaCreacion = fechaCreacion;
-    }
 }
