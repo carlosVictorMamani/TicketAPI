772b7ade1e55fa0fb61c2fb085f771d8120495e2
Carlos Victor Mamani <cvmamani@indracompany.com>
2025-09-03 22:49:51 -0500

    Prueba 2054v2 - Desarrollo

--- src/main/java/com/minsait/comunidad/ticket/domain/Ticket.java
+++ src/main/java/com/minsait/comunidad/ticket/domain/Ticket.java
@@ -23,21 +23,14 @@ public class Ticket{
     
     @Id
     private ObjectId id;
-    @Nullable
+    
     private String codigo;
-    @Nullable
     private long orden;
-    @Nullable
     private String comentario;
-    @Nullable
     private String descripcion;
-    @Nullable
     private String solicitante;
-    @Nullable
     private String usuarioGenerador;
-    @Nullable
     private Estado estado;
-    @Nullable
     private LocalDateTime fechaCreacion;
 
 }
